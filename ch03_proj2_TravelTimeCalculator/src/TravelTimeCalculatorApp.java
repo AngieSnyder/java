1b756c0e0320438c32a88738ce2e95345f6aa1d2
AngieSnyder <Angie.Snyder@protonmail.com>
2018-09-07 19:45:48 -0400

    Ch03 proj2 Travel time calculator

---
+++ ch03_proj2_TravelTimeCalculator/src/TravelTimeCalculatorApp.java
@@ -0,0 +1,45 @@
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class TravelTimeCalculatorApp {
+
+	public static void main(String[] args) {
+		System.out.println();
+		System.out.println("Welcome to the Travel Time Calculator");
+		Scanner sc = new Scanner(System.in);
+		String choice = "y";
+		System.out.println();
+		
+		//prompt user imput
+		while (choice.equalsIgnoreCase("y")) {
+			System.out.print("Enter miles: ");
+			double miles = sc.nextDouble();
+			System.out.print("Enter miles per hour: ");
+			double mph = sc.nextDouble();
+			System.out.println();
+		
+		//calculate time
+			int hours = (int) (miles / mph) ;
+			int minutes = (int) (miles % mph);
+			
+		//output time
+			NumberFormat number = NumberFormat.getNumberInstance();
+			//number.setMaximumFractionDigits(0);
+			
+		String message =  "Estimated travel time"  +"\n"
+				+"-----------------------"+"\n"
+				+ "Hours: " + number.format(hours)   +"\n"
+				+ "Minutes: " + number.format(minutes) +"\n";
+		System.out.println(message);		
+		
+		
+		System.out.print("Continue? (y/n)");
+		choice = sc.next();
+		System.out.println();
+		}
+		System.out.println("Goodbye");
+		sc.close();
+
+	}
+
+}
