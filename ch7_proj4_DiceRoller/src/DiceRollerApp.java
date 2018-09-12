0c7c519dfdff2789951cc14a5ebe941e7c9c763d
AngieSnyder <Angie.Snyder@protonmail.com>
2018-09-12 14:11:26 -0400

    updated DiceRollerApp

--- ch7_proj4_DiceRoller/src/DiceRollerApp.java
+++ ch7_proj4_DiceRoller/src/DiceRollerApp.java
@@ -4,10 +4,13 @@ public class DiceRollerApp {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Dice Roller!");
+		System.out.println();
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.print("Roll the dice? (y/n): ");
 		String choice =sc.next();
+		System.out.println();
+		
 		while (choice.equalsIgnoreCase("y")) {
 			Dice d = new Dice();
 			System.out.println(d);
