e5a15cabad968e11f5818bf821a9d3ec2fee6bb6
AngieSnyder <Angie.Snyder@protonmail.com>
2018-09-25 11:54:02 -0400

    updated

--- chapter15/ch15proj1CountryListManager/src/CountryListManagerApp.java
+++ chapter15/ch15proj1CountryListManager/src/CountryListManagerApp.java
@@ -6,47 +6,62 @@ public class CountryListManagerApp {
 		System.out.println("Welcome to the Country List Manager");
 		CountryIO cio = new CountryIO();
 		String command = "";
-		while(!command.equalsIgnoreCase("4")) {
+		while (!command.equalsIgnoreCase("4")) {
 			displayCommandMenu();
 			command = Console.getString("Enter menu #: ");
-			if(command.equals("1")) {
+			if (command.equals("1")) {
 				List<String> countries = cio.getAll();
-				if (countries==null||countries.size()==0) {
+				if (countries == null || countries.size() == 0) {
 					System.out.println("Countries file doesn't exist. Please enter.");
+				} else {
+					for (String c : countries) {
+						System.out.println(c);
+					}
+					System.out.println();
 				}
-				else {
-					for (String c: countries) {
-					System.out.println(c);
-				}
-				System.out.println();
-			}
+			} 
 			else if (command.equals("2")) {
+				// add country
 				int id = Console.getInt("Enter Country id: ");
 				String code = Console.getString("Enter country code: ");
 				String name = Console.getString("Enter country name: ");
 				Country c = new Country(id, code, name);
 				if (cio.add(c)) {
-				System.out.println("This country has been saved");
+					System.out.println("This country has been saved");
+				} 
+				else {
+					System.out.println("Error adding country.");
+				}
+				System.out.println();
+			} 
+			else if (command.equals("3")) {
+				// remove country
+				int id = Console.getInt("Enter country id to remove: ");
+				Country c = cio.get(id);
+				if (cio.remove(c)) {
+					System.out.println(c.getCode() + " successfully removed.");
+				} 
+				else {
+					System.out.println("Error occurred.");
+				}
+				System.out.println();
+			} 
+			else if (!command.equals("4")) {
+				// invalid command
+				System.out.println("Invalid command.  Please try again.");
 			}
-			}
-			else {
-				System.out.println("Error adding country.");
-			}
-			System.out.println();
+
 		}
-		}
-			else if 
-		
-		
-		
 		System.out.println("Bye");
 
 	}
-	
-	private void displayCommandMenu("COMMAND MENU\n" +
-					"1 - List countries\n" + 
-					"2 - Add a country\n" + 
-					"3 - Remove a country\n" +
-					"4 - Exit");
 
+	private static void displayCommandMenu() {
+		System.out.println("COMMAND MENU\n" +
+				"1 - List countries\n" + 
+				"2 - Add a country\n" + 
+				"3 - Remove a country\n" + 
+				"4 - Exit");
+
+	}
 }
